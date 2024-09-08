e75391adb95a7cb2d87d6062c64fcfa9d5240694
ronFeldman <ronfeldman2003@gmail.com>
2024-09-08 20:33:58 +0300

    update test to use with jenkins

--- e2e.py
+++ e2e.py
@@ -1,21 +1,27 @@
-import sys
-
-import selenium.common.exceptions
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
+import sys
 
 
 def test_scores_service(url):
-    dr = webdriver.Chrome()
-    dr.get(url)
-    try:
-        score = int(WebDriverWait(dr, 10).until(EC.presence_of_element_located((By.ID, 'score'))).text)
-        return 1 <= score <= 1000
-    except ValueError:
-        return False
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    service = Service(executable_path='/usr/local/bin/chromedriver')
+
+    with webdriver.Chrome(service=service, options=chrome_options) as driver:
+        driver.get(url)
+        try:
+            score = int(WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'score'))).text)
+            return 1 <= score <= 1000
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
 
 
 def main_function(url):
